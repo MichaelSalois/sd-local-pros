1077a18c3bfb8cada32c59e387e8a876278ad6c1
Michael Salois <michael@saloisdigital.com>
2026-02-17 16:25:12 -0800

    Add GA4 and Meta Pixel tracking

--- {src/{app/layout.tsx
+++ {src/{app/layout.tsx
@@ -1,4 +1,4 @@
-﻿import type { Metadata } from 'next';
+import type { Metadata } from 'next';
 import Script from 'next/script';
 import './globals.css';
 import Navbar from '@/components/layout/Navbar';
@@ -59,19 +59,19 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <Script
-          src={https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}}
+          src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
           strategy="afterInteractive"
         />
         <Script id="google-analytics" strategy="afterInteractive">
-          {
+          {`
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
             gtag('config', '${GA_MEASUREMENT_ID}');
-          }
+          `}
         </Script>
         <Script id="meta-pixel" strategy="afterInteractive">
-          {
+          {`
             !function(f,b,e,v,n,t,s)
             {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
             n.callMethod.apply(n,arguments):n.queue.push(arguments)};
@@ -82,14 +82,14 @@ export default function RootLayout({
             'https://connect.facebook.net/en_US/fbevents.js');
             fbq('init', '${META_PIXEL_ID}');
             fbq('track', 'PageView');
-          }
+          `}
         </Script>
         <noscript>
           <img
             height="1"
             width="1"
             style={{ display: 'none' }}
-            src={https://www.facebook.com/tr?id=${META_PIXEL_ID}&ev=PageView&noscript=1}
+            src={`https://www.facebook.com/tr?id=${META_PIXEL_ID}&ev=PageView&noscript=1`}
             alt=""
           />
         </noscript>
